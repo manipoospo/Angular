f2a74713f8f69c8812ea3972b60df9e09a85b375
manipoospo <manipoo1991@gmail.com>
2019-08-20 10:44:39 +0530

    add message component

--- chart/src/app/app.module.ts
+++ chart/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MessageBoxComponent } from './message-box/message-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageBoxComponent
   ],
   imports: [
     BrowserModule,
